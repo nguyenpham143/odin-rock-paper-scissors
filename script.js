cae23157d05a383b364397087a3599bc9cd638a5
nguyenpham143 <nguyenphamdz1403@gmail.com>
2026-09-24 13:11:09 +0000

    add: get computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    if (randomNumber >= 0 && randomNumber < 1 / 3) return "rock";
+    else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) return "paper";
+    else return "scissors";
+}
