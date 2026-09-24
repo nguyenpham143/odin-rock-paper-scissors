13e11ab357043fcdf876362ca8894533cbf1f7ea
nguyenpham143 <nguyenphamdz1403@gmail.com>
2026-09-24 13:32:37 +0000

    add: playRound function

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let input = prompt("Choose rock, paper or scissors to play: ");
+    input = input.toLowerCase();
     if (input === "rock") return "rock";
     else if (input === "paper") return "paper";
     else if (input === "scissors") return "scissors";
@@ -15,3 +16,37 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            console.log("It's a tie!");
+        } else if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock");
+            computerScore++;
+        } else {
+            console.log("You win! Rock beats Scissors");
+            humanScore++;
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock");
+            humanScore++;
+        } else if (computerChoice === "paper") {
+            console.log("It's a tie!");
+        } else {
+            console.log("You lose! Scissors beats Paper");
+            computerScore++;
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats scissors");
+            computerScore++;
+        } else if (computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper");
+            humanScore++;
+        } else {
+            console.log("It's a tie!");
+        }
+    }
+}
