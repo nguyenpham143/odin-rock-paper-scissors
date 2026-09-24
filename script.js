0b0e166e96a4b183110871d942fc71c6297d5ae9
nguyenpham143 <nguyenphamdz1403@gmail.com>
2026-09-24 13:19:00 +0000

    add: get human choice function

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function getComputerChoice() {
     else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) return "paper";
     else return "scissors";
 }
+
+function getHumanChoice() {
+    let input = prompt("Choose rock, paper or scissors to play: ");
+    if (input === "rock") return "rock";
+    else if (input === "paper") return "paper";
+    else if (input === "scissors") return "scissors";
+    else return "";
+}
+
+let humanScore = 0;
+let computerScore = 0;
